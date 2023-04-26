be7a7296d293eb90cce3dd20d05ceed58aa95837
rasikapatil3499 <66173415+rasikapatil3499@users.noreply.github.com>
2023-04-26 15:13:26 +0530

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If PictureBox1.Visible = True Then
+            PictureBox1.Visible = False
+            PictureBox2.Visible = True
+        ElseIf PictureBox2.Visible = True Then
+            PictureBox2.Visible = False
+            PictureBox3.Visible = True
+        ElseIf PictureBox3.Visible = True Then
+            PictureBox3.Visible = False
+            PictureBox1.Visible = True
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
